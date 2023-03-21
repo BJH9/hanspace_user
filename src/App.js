2ea376fdac933e20c09c18303c0526135b653d6c
lukehongg <lukehongg@handong.ac.kr>
2023-03-21 14:18:21 +0900

    Google OAuth login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Router from './Router';
+import { useEffect } from 'react';
+import GoogleLogin from './components/GoogleLogin';
 
 function App() {
   return (
     <>
+      <GoogleLogin />
       <div className="App">HanSpace - User</div>
       <Router />
     </>
